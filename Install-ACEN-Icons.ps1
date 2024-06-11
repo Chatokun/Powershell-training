f69e2e36a7449d0e718d4464a6975baed823de35
Chatokun <chatoyer.huggins@gmail.com>
2024-06-11 15:24:46 -0400

    Added Policy Cleaned up script

--- Install-ACEN-Icons.ps1
+++ Install-ACEN-Icons.ps1
@@ -1,7 +1,6 @@
 #This file aims to consolidate the icon Intune programs for ACEN
 
 
-
 #Installation Script: Icons
 
 $Icon1 = "Question (1).ico"
@@ -14,21 +13,28 @@ if(-not(Test-Path "C:\Programdata\Acen Icons")){
     New-Item -Path "C:\Programdata\Acen Icons" -ItemType Directory -Force
 }
 
-Copy-Item -Path "$ScriptPath\$FileName1" -Destination "C:\Programdata\Acen Icons\$Icon1"
+Copy-Item -Path "$ScriptPath\$Icon1" -Destination "C:\Programdata\Acen Icons\$Icon1"
 
-Copy-Item -Path "$ScriptPath\$FileName2" -Destination "C:\Programdata\Acen Icons\$Icon2"
+Copy-Item -Path "$ScriptPath\$Icon2" -Destination "C:\Programdata\Acen Icons\$Icon2"
 
 
 #Installation Script: How to Link
 
 if (Test-path -Path "C:\Program Files\Google\Chrome\Application"){$Howto = "How To.lnk"}
-elseif (Test-path -Path "C:\Program Files (x86)\Google\Chrome\Application"){$How Tox86.lnk"}
+elseif (Test-path -Path "C:\Program Files (x86)\Google\Chrome\Application"){$Howto = "How Tox86.lnk"}
+else {Write-Output "Chrome not installed"} 
 
 $ScriptPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 
 Copy-Item -Path "$ScriptPath\$Howto" -Destination "$Env:Public\Desktop\How To.lnk"
 
 
-#Installation Script:  
+#Installation Script: Policy and Procedure Link
 
+if (Test-path -Path "C:\Program Files\Google\Chrome\Application"){$Policy = "ACEN-Policy & Procedure.lnk"}
+elseif (Test-path -Path "C:\Program Files (x86)\Google\Chrome\Application"){$Policy = "ACEN-Policy & Procedurex86.lnk"}
+else {Write-Output "Chrome not installed"} 
 
+$ScriptPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+
+Copy-Item -Path "$ScriptPath\$Policy" -Destination "$Env:Public\Desktop"
