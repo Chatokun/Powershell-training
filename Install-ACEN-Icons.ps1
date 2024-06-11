472411f07567e4a10b7af879d68a2ab0eb2fc8dc
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-06-11 14:58:22 -0400

    Update Install-ACEN-Icons.ps1
    Removed detect scripts
    Setup method to detect where Chrome is installed

--- Install-ACEN-Icons.ps1
+++ Install-ACEN-Icons.ps1
@@ -2,11 +2,11 @@
 
 
 
-#Installation Script: Install-file.ps1
+#Installation Script: Icons
 
-$FileName1 = "Question (1).ico"
+$Icon1 = "Question (1).ico"
 
-$FileName2 = "User manual (1).ico"
+$Icon2 = "User manual (1).ico"
 
 $ScriptPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 
@@ -14,36 +14,21 @@ if(-not(Test-Path "C:\Programdata\Acen Icons")){
     New-Item -Path "C:\Programdata\Acen Icons" -ItemType Directory -Force
 }
 
-Copy-Item -Path "$ScriptPath\$FileName1" -Destination "C:\Programdata\Acen Icons\$FileName1"
+Copy-Item -Path "$ScriptPath\$FileName1" -Destination "C:\Programdata\Acen Icons\$Icon1"
 
-Copy-Item -Path "$ScriptPath\$FileName2" -Destination "C:\Programdata\Acen Icons\$FileName2"
+Copy-Item -Path "$ScriptPath\$FileName2" -Destination "C:\Programdata\Acen Icons\$Icon2"
 
 
-#Installation Script: Install-file.ps1
+#Installation Script: How to Link
 
-$FileName = "How To.lnk"
+if (Test-path -Path "C:\Program Files\Google\Chrome\Application"){$Howto = "How To.lnk"}
+elseif (Test-path -Path "C:\Program Files (x86)\Google\Chrome\Application"){$How Tox86.lnk"}
 
 $ScriptPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 
-Copy-Item -Path "$ScriptPath\$FileName" -Destination "$Env:Public\Desktop"
+Copy-Item -Path "$ScriptPath\$Howto" -Destination "$Env:Public\Desktop\How To.lnk"
 
-#Detect File : Detect-file.ps1
 
-$FileName = "How To.lnk"
+#Installation Script:  
 
-if (Test-Path -Path "$Env:Public\Desktop\$FileName"){Write-Output "0"}
-
-    
-
-#Detect File : Detect-file.ps1
-
-$FileName1 = "Question (1).ico"
-
-$FileName2 = "User manual (1).ico"
-
-$Path1Test = Test-Path -Path "C:\Programdata\Acen Icons\$FileName1"
-
-$Path2Test = Test-Path -Path "C:\Programdata\Acen Icons\$FileName2"
-
-if ($path1test -eq "True" -and $Path2Test -eq "True"){Write-Output "0"}
 
