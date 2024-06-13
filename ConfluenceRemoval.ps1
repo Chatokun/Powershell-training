0fe6d16ce379d735d56604b809536473b19a2d5b
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-06-13 11:08:52 -0400

    Update ConfluenceRemoval.ps1 Corrected path issue

--- ConfluenceRemoval.ps1
+++ ConfluenceRemoval.ps1
@@ -3,4 +3,4 @@
 Remove-Item -Path "$Env:Public\Desktop\How To.lnk"
 Remove-Item -Path "$Env:Public\Desktop\ACEN-Policy & Procedure.lnk"
 Remove-Item -Path "C:\Programdata\Acen Icons\Question (1).ico"
-Remove-Item -Path "User manual (1).ico"
+Remove-Item -Path "C:\Programdata\Acen Icons\User manual (1).ico"
