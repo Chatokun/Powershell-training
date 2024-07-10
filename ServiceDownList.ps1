574ac678e2a128a283686d66bce9a00c77043dd5
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-07-10 14:32:51 -0400

    Changed to function

--- ServiceDownList.ps1
+++ ServiceDownList.ps1
@@ -1,7 +1,13 @@
 #This is a script to get a list of services that are offline
 
-# Get the list of services that are offline
-$services = Get-Service | Where-Object {$_.Status -eq "Stopped"}
+#Make a function to get the list of services that are offline
+function Get-ServiceDownList {
+    # Get the list of services that are offline
+    $services = Get-Service | Where-Object {$_.Status -eq "Stopped"}
+
+    # Output the list of services that are offline
+    $services
+}
 
 # Output the list of services that are offline
 $services
