d47c4b4c7ade6a9b9aec20eae449a8bed1e1d84b
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-10-02 10:05:23 -0400

    New Script

---
+++ BlockOutlookLogging.ps1
@@ -0,0 +1,25 @@
+# Get all Users
+
+$Users = Get-ChildItem -Path "$($ENV:SystemDrive)\Users"
+
+# Process all the Users
+
+$Users | ForEach-Object {
+
+      Write-Host "Processing user: $($_.Name)" -ForegroundColor Cyan
+
+      $path = "$($ENV:SystemDrive)\Users\$($_.Name)\AppData\Local\Temp\Outlook Logging\"
+
+      If (Test-Path $path) {
+
+        Write-host "Removing log files from $path" -ForegroundColor Cyan
+
+        Remove-Item -Path $path -Recurse
+
+        Write-host "Creating dummy file to prevent log files" -ForegroundColor Cyan
+
+        New-Item -Path "$($ENV:SystemDrive)\Users\$($_.Name)\AppData\Local\Temp" -Name "Outlook Logging" -ItemType File
+
+      }
+
+}
