997f5bc0551cca831aa2ba7c004b5c6d1c29dbc8
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-10-17 14:35:24 -0400

    Cleanup

--- UninstallAllBluebeam.ps1
+++ UninstallAllBluebeam.ps1
@@ -2,6 +2,9 @@
 $BluebeamGUIDs = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "Bluebeam*"} | Select-Object -ExpandProperty IdentifyingNumber
 
 #For each GUID in $BluebeamGUIDs, uninstall the software
-if ($BluebeamGUIDs) | foreach ($GUID in $BluebeamGUIDs) {
-    Start-Process -FilePath "C:\windows\system32\msiexec.exe" -ArgumentList "/x $GUID /qn /log C:\Savant\BluebeamUninstall.log" -Wait
+if ($BluebeamGUIDs) {
+    foreach ($GUID in $BluebeamGUIDs) {
+        Start-Process -FilePath "C:\windows\system32\msiexec.exe" -ArgumentList "/x $GUID /qn /log C:\Savant\BluebeamUninstall.log" -Wait
+    }
 }
+
