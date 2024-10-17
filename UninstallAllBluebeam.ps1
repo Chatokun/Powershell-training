cae3e8a99a4a5c0e0ebac2eff7057e2b66cd2b84
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-10-17 14:27:55 -0400

    New script

---
+++ UninstallAllBluebeam.ps1
@@ -0,0 +1,7 @@
+#Pull all GUIDs of any software starting with the name Bluebeam and put it into a variable called $BluebeamGUIDs
+$BluebeamGUIDs = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "Bluebeam*"} | Select-Object -ExpandProperty IdentifyingNumber
+
+#For each GUID in $BluebeamGUIDs, uninstall the software
+foreach ($GUID in $BluebeamGUIDs) {
+    Start-Process -FilePath "C:\windows\system32\msiexec.exe" -ArgumentList "/x $GUID /qn" -Wait
+}
