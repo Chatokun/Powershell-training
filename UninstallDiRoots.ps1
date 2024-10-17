5ef3e6aaed00226e6d46aea32e7238fe5bf3a22e
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-10-17 13:09:58 -0400

    More cleanup

--- UninstallDiRoots.ps1
+++ UninstallDiRoots.ps1
@@ -2,4 +2,4 @@
 $ID = get-wmiobject Win32_Product | where-object {$_.name -eq "DiRoots.One"}
 
 #Uninstall the product
-if $id { Start-Process "C:\windows\system32\msiexec.exe" -ArgumentList "/x $($id.identifyingnumber) /q  /l c:\savant\DiRootUninstall.log"
+if ($id) { Start-Process "C:\windows\system32\msiexec.exe" -ArgumentList "/x $($id.identifyingnumber) /q  /l c:\savant\DiRootUninstall.log" }
