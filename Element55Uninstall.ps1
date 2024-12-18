762dee32f50615ed0c578419d76c91c1cfb346dc
Chatokun <135379052+Chatokun@users.noreply.github.com>
2024-12-18 13:28:45 -0500

    New Script to uninstall Element55

---
+++ Element55Uninstall.ps1
@@ -0,0 +1,6 @@
+#Find the installed version of Element55 Time Capture Agent
+$Element55 = get-wmiobject Win32_Product | where-object {$_.name -eq "Element55 Time Capture Agent
+"}
+
+#Uninstall the product
+if ($Element55) { Start-Process "C:\windows\system32\msiexec.exe" -ArgumentList "/x $($Element55.identifyingnumber) /q  /l c:\savant\Element55Uninstall.log" }
