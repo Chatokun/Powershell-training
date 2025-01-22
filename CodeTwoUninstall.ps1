f7902929a52671296d4093df39875e1adb3a5cb7
Chatokun <135379052+Chatokun@users.noreply.github.com>
2025-01-22 15:56:46 -0500

    New Scripts

---
+++ CodeTwoUninstall.ps1
@@ -0,0 +1,10 @@
+#This Script will uninstall all versions of CodeTwo Email Signatures
+
+#Get all versions of CodeTwo and put their GUIDs in a variable named $CODETWO
+$CODETWO = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "CodeTwo*" }
+
+#Uninstall all versions of CodeTwo using msiexec /x and the GUIDs stored in $CODETWO and store result in $result
+$result = $CODETWO | ForEach-Object { msiexec /x $_.IdentifyingNumber /qn } 
+
+#Export the results to C:\Savant\Logs\UninstallCodeTwo.txt
+$result | Out-File -FilePath "C:\Savant\Logs\UninstallCodeTwo.txt" -Append
---
+++ MailboxandCalendarPermissions.ps1
@@ -0,0 +1,40 @@
+# Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName savantadmin@huffpowellbailey.com -ShowProgress $true
+
+# Get a list of all mailboxes
+$mailboxes = Get-Mailbox -ResultSize Unlimited
+
+# Initialize an array to store results
+$delegatedAccessList = @()
+
+# Loop through each mailbox to get the mailbox permissions
+foreach ($mailbox in $mailboxes) {
+    # Get Full Access permissions for each mailbox
+    $permissions = Get-MailboxPermission -Identity $mailbox.Identity -ResultSize Unlimited
+
+
+    # If there are permissions, add them to the list
+    foreach ($permission in $permissions) {
+        $delegatedAccessList += [PSCustomObject]@{
+            Mailbox     = $mailbox.DisplayName
+            User        = $permission.User
+            AccessRights = $permission.AccessRights
+            Inherited   = $permission.IsInherited
+            Folder = $permission.FolderName
+        }
+    
+    #Check the permissions of the calendar folder for each mailbox as well and add them to the array
+    $calendarPermissions = Get-MailboxFolderPermission -Identity "$($mailbox.UserPrincipalName):\Calendar" -ResultSize Unlimited
+    foreach ($permission in $calendarPermissions) {
+        $delegatedAccessList += [PSCustomObject]@{
+            Mailbox     = $mailbox.DisplayName
+            User        = $permission.User
+            AccessRights = $permission.AccessRights
+            Inherited   = $permission.IsInherited
+            Folder = $permission.FolderName
+        }
+    }
+}
+
+# Export results to a CSV file
+$delegatedAccessList | Export-Csv -Path "C:\Users\chatoyer.huggins\OneDrive - savantcts.com\Clients\HPB\FullAccessDelegates.csv" -NoTypeInformation
