52d9c3ef669d52c7c10ac7085c7e4faecec19322
Chatokun <135379052+Chatokun@users.noreply.github.com>
2025-06-16 10:35:00 -0400

    Corrected webhook url

--- OTPScrpt.ps1
+++ OTPScrpt.ps1
@@ -3,7 +3,7 @@ Import-Module OTPauth
 # Generate the current TOTP code 
 $code = get-otpauthcredential -issuer "Google" | get-otpauthcode 
 # Teams webhook URL 
-$webhookUrl = "https://outlook.office.com/webhook/..."  
+$webhookUrl = "https://gregorydoylefirm.webhook.office.com/webhookb2/0f86d23b-abf8-434b-a213-732e4548bb28@a6af6802-14e3-45e2-8a7e-19167e90cf7a/IncomingWebhook/51889af3185948b2873d156d65d05475/f3e19ffa-b16c-4ba5-b293-5899dc37c596/V27J7G0DS8Tkm5jUjLcsGBRXO7x-Wb2_FJZWi53C11RKc1"  
 # Replace with your Teams webhook 
 # Create the JSON body 
 $body = @{     text = "Gmail MFA Code: $code" } | ConvertTo-Json 
