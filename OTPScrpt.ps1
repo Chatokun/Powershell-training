ff940dac542f97d49a6c4ac9c71fa5ca7281ad99
Chatokun <135379052+Chatokun@users.noreply.github.com>
2025-06-19 16:00:05 -0400

    Changed back to old webhook which was working, and set it to open credential

--- OTPScrpt.ps1
+++ OTPScrpt.ps1
@@ -1,8 +1,10 @@
 # Load the OTP module 
 Import-Module OTPauth 
+#Open Credential Store using Open-OtpAuthCredentialStore
+Open-OtpAuthCredentialStore -password <get this from ITGlue>
 # Generate the current TOTP code 
 $code = get-otpauthcredential -issuer "Google" | get-otpauthcode 
-# Teams webhook URL 
+# Teams webhook URL
 $webhookUrl = "https://gregorydoylefirm.webhook.office.com/webhookb2/0f86d23b-abf8-434b-a213-732e4548bb28@a6af6802-14e3-45e2-8a7e-19167e90cf7a/IncomingWebhook/51889af3185948b2873d156d65d05475/f3e19ffa-b16c-4ba5-b293-5899dc37c596/V27J7G0DS8Tkm5jUjLcsGBRXO7x-Wb2_FJZWi53C11RKc1"  
 # Replace with your Teams webhook 
 # Create the JSON body 
