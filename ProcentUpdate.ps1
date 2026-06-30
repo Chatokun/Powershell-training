c16f2e53de2ea9906c2b2b973bfcfc8397243377
Chatokun <135379052+Chatokun@users.noreply.github.com>
2026-06-30 11:55:32 -0400

    Changed detection method

--- ProcentUpdate.ps1
+++ ProcentUpdate.ps1
@@ -1,20 +1,20 @@
 $logFile = "$env:LOCALAPPDATA\ProcentLogonTest.log"
+$tokenFile = "$env:LOCALAPPDATA\ProcentUpdate.token"
+$today = (Get-Date).ToString("yyyy-MM-dd")
 
 "[$(Get-Date)] Logon script STARTED" | Out-File $logFile -Append
 
-# Check last logon time
-$lastLogon = Get-WmiObject Win32_NetworkLoginProfile | 
-    Where-Object { $_.Name -eq "$env:USERDOMAIN\$env:USERNAME" } | 
-    Select-Object -ExpandProperty LastLogon
-
-if ($lastLogon) {
-    $lastLogonTime = [Management.ManagementDateTimeConverter]::ToDateTime($lastLogon)
-    $timeSinceLogon = (Get-Date) - $lastLogonTime
-    
-    if ($timeSinceLogon.TotalMinutes -gt 5) {
-        "[$(Get-Date)] User has not logged off from previous session (last logon: $lastLogonTime)" | Out-File $logFile -Append
+# Check if token exists for today
+if (Test-Path $tokenFile) {
+    $tokenDate = Get-Content $tokenFile
+    if ($tokenDate -eq $today) {
+        "[$(Get-Date)] Token found for today, skipping execution" | Out-File $logFile -Append
         "[$(Get-Date)] Logon script FINISHED" | Out-File $logFile -Append
         exit
+    }
+    else {
+        "[$(Get-Date)] Removing old token from $tokenDate" | Out-File $logFile -Append
+        Remove-Item $tokenFile -Force
     }
 }
 
@@ -38,20 +38,26 @@ while ($attempt -lt $maxAttempts) {
 }
 
 if (-not $driveExists) {
-    "[$(Get-Date)] Q drive not found after $maxAttempts attempts, continuing anyway" | Out-File $logFile -Append
+    "[$(Get-Date)] Q drive not found after $maxAttempts attempts, exiting" | Out-File $logFile -Append
+    "[$(Get-Date)] Logon script FINISHED" | Out-File $logFile -Append
+    exit
 }
-
-"[$(Get-Date)] Logon script FINISHED" | Out-File $logFile -Append
 
 $programPath = "Q:\Program Files\ProcentSQL\UpdateProcent_PMMgr.accdb"
 $workingDir  = "Q:\Program Files\ProcentSQL"
-
 $computerName = $env:COMPUTERNAME
+
 if ($computerName -ne "CF-RDS01V") {
     exit
 }
 
 if (Test-Path $programPath) {
     Start-Process -FilePath $programPath -WorkingDirectory $workingDir
-    "[$(Get-Date)] Update script FINISHED" | Out-file $logFile -Append
+    "[$(Get-Date)] Update script FINISHED" | Out-File $logFile -Append
+    
+    # Create token for today on success
+    $today | Out-File $tokenFile -Force
+    "[$(Get-Date)] Token created for $today" | Out-File $logFile -Append
 }
+
+"[$(Get-Date)] Logon script FINISHED" | Out-File $logFile -Append
